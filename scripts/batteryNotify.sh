ee24dfcee16be28d1076bfd70e84601901a5254f
vmgabriel <vmgabriel96@gmail.com>
2019-03-18 01:07:43 -0500

    Agregada notificacion de bateria

---
+++ scripts/batteryNotify.sh
@@ -0,0 +1,20 @@
+# ! /bin/bash
+
+# Permite mandar un dialogo que avise si la bateria esta completa o esta vacia
+# Autor: Gabriel Vargas Monroy - tw: v_mgabriel
+
+expresion=$(/usr/bin/upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | grep -Eo "([0-9]{1,3})")
+expresion=$(($expresion + 0))
+load=$(/usr/bin/upower -i $(upower -e | grep 'BAT') | grep -E "state" | grep -Eo "charging|discharging")
+
+if [ $expresion -ge 96 ] && [ $load -eq "charging" ]; then
+    /usr/bin/dunstify --appname="Battery Notify" --urgency=1 "<b>Bateria Completa</b>" "La bateria se encuentra en el $expresion%"
+fi
+
+if [ $expresion -lt 16 ] && [ $expresion -ge 10 ] && [ $load -eq "discharging" ]; then
+    /usr/bin/dunstify --appname="Battery Notify" --urgency=1 "<b>Bateria Limitada</b>" "La bateria se encuentra en el minimo deseado: $expresion%"
+fi
+
+if [ $expresion -le 9 ] && [ $load -eq "discharging" ]; then
+    /usr/bin/dunstify --appname="Battery Notify" --urgency=0 "<b>Bateria Critica</b>" "La bateria se Encuentra en Estado muy Bajo $expresion%, por favor conectarlo"
+fi
